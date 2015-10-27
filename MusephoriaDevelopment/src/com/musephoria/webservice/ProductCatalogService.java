dedcde97a4f5bedbd4cd4743a5b89a0869ad6b61
Vivian Ajay Monis <monis256@gmail.com>
2015-10-26 22:07:36 -0400

    Including the product catalog service.java file

---
+++ MusephoriaDevelopment/src/com/musephoria/webservice/ProductCatalogService.java
@@ -0,0 +1,47 @@
+/**
+ *
+ */
+package com.musephoria.webservice;
+
+import java.util.List;
+
+import com.musephoria.dao.CdHome;
+import com.musephoria.entity.Cd;
+
+/**
+ * @author Admin
+ *
+ */
+public class ProductCatalogService implements IProductCatalogService {
+
+	/* (non-Javadoc)
+	 * @see com.musephoria.webservice.IProductCatalogService#getCategoryList()
+	 */
+	@Override
+	public List<String> getCategoryList() {
+		CdHome cdDao = new CdHome();
+		List<String> categoryList = cdDao.getCategoryList();
+		return categoryList;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.musephoria.webservice.IProductCatalogService#getProductList(java.lang.String)
+	 */
+	@Override
+	public List<Cd> getProductList(String categoryId) {
+		CdHome cdDao = new CdHome();
+		List<Cd> productList = cdDao.getProductList(categoryId);
+		return productList;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.musephoria.webservice.IProductCatalogService#getProductInfo(int)
+	 */
+	@Override
+	public Cd getProductInfo(int productid) {
+		CdHome cdDao = new CdHome();
+		Cd productInfo = cdDao.getProductInfo(productid);
+		return productInfo;
+	}
+
+}
