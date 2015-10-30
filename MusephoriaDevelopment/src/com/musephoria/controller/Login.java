e0562eb2b6882f3b1866531d96ebd8c394db7829
Charat <CharatKakkar>
2015-10-30 02:06:33 -0400

    Added Login Servlet

---
+++ MusephoriaDevelopment/src/com/musephoria/controller/Login.java
@@ -0,0 +1,75 @@
+package com.musephoria.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.musephoria.util.Constants;
+import com.musephoria.webserviceclient.*;
+import com.musephoria.webserviceclient.OrderProcessServiceStub.Customer;
+import com.musephoria.webserviceclient.OrderProcessServiceStub.Result;
+
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Customer custObj = null;
+		int custId;
+		
+		
+		String username = (String)request.getAttribute("username");
+		request.getSession().setAttribute("username", username);
+		String password= (String) request.getAttribute("Password");
+		request.getSession().setAttribute("password", password);
+		
+		OrderProcessServiceClient client = new OrderProcessServiceClient();
+		Result resObj = client.getAccount(username, password, null);
+		
+		 if (resObj.getStatusMessage().equals(Constants.userNameDoesntExist))
+		 {
+			 request.getSession().setAttribute("Message","Username doesn't Exists");
+			 request.getRequestDispatcher("Login.jsp");
+		 }
+		 
+		 else if (resObj.getStatusMessage().equals(Constants.userNamePasswordMismatch))
+		 {
+			 request.getSession().setAttribute("Message","Either username or Password is incorrect");
+			 request.getRequestDispatcher("Login.jsp");
+		 }
+		 
+		 else
+		 {   
+			 custObj = (Customer) resObj.getResultList();			 
+			 custId=custObj.getCustomerId();
+			 request.getSession().setAttribute("custId",custId);			 
+			 request.getRequestDispatcher("Home.jsp");
+		 }
+	}
+
+}
